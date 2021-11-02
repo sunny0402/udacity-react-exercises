f9a11d607251c68c1b7a11947c675f8e287ddf7c
Alex Volsky <alexvolsky@Sergueis-Mac-mini.local>
2021-11-02 15:42:47 -0400

    map method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
@@ -11,94 +11,102 @@ For detailed instructions, refer to instructions.md.
 const profiles = [
   {
     id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
+    userID: "1",
+    favoriteMovieID: "1",
   },
   {
     id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
+    userID: "2",
+    favoriteMovieID: "1",
   },
   {
     id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
+    userID: "4",
+    favoriteMovieID: "5",
   },
   {
     id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
+    userID: "5",
+    favoriteMovieID: "2",
   },
   {
     id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
+    userID: "3",
+    favoriteMovieID: "5",
   },
   {
     id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
+    userID: "6",
+    favoriteMovieID: "4",
   },
 ];
 
 const users = {
   1: {
     id: 1,
-    name: 'Jane Cruz',
-    userName: 'coder',
+    name: "Jane Cruz",
+    userName: "coder",
   },
   2: {
     id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
+    name: "Matthew Johnson",
+    userName: "mpage",
   },
   3: {
     id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
+    name: "Autumn Green",
+    userName: "user123",
   },
   4: {
     id: 4,
-    name: 'John Doe',
-    userName: 'user123',
+    name: "John Doe",
+    userName: "user123",
   },
   5: {
     id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
+    name: "Lauren Carlson",
+    userName: "user123",
   },
   6: {
     id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
+    name: "Nicholas Lain",
+    userName: "user123",
   },
 };
 
 const movies = {
   1: {
     id: 1,
-    name: 'Planet Earth 1',
+    name: "Planet Earth 1",
   },
   2: {
     id: 2,
-    name: 'Selma',
+    name: "Selma",
   },
   3: {
     id: 3,
-    name: 'Million Dollar Baby',
+    name: "Million Dollar Baby",
   },
   4: {
     id: 4,
-    name: 'Forrest Gump',
+    name: "Forrest Gump",
   },
   5: {
     id: 5,
-    name: 'Get Out',
+    name: "Get Out",
   },
 };
 
 class App extends Component {
   render() {
+    profiles.forEach((elem) => {
+      console.log(
+        "elem.userID: " +
+          elem.userID +
+          " elem.favoriteMovieID: " +
+          elem.favoriteMovieID
+      );
+    });
     return (
       <div>
         <header className="App-header">
@@ -106,6 +114,16 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+        <ul>
+          {profiles.map((a_profile) => {
+            return (
+              <li key={a_profile.id}>{`${
+                users[a_profile.userID].name
+              }'s favourite movie is
+                          ${movies[a_profile.favoriteMovieID].name}.`}</li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
