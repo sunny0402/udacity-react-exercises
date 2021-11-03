876f89a7637bee09c3214435206568e4f6b6bd9e
Alex Volsky <alexvolsky@Sergueis-Mac-mini.local>
2021-11-03 11:45:30 -0400

    create a likes list

--- README.md
+++ README.md
@@ -2,40 +2,32 @@
 
 # Instructions
 
-Use React and the `profiles`, `users`, and `movies` data in `App.js` to display a list of users alongside their favorite movies.
+Let's do something a little bit more complicated. Instead of displaying a
+list of users and their movies, this time you need to display a list of movies.
+
+For each movie in the list, there are two options:
+
+1. If the movie has been favorited, then display a list of all of the users who said that this movie was their favorite.
+2. If the movie has _not_ been favorited, display some text stating that no one favorited the movie.
+
+As you go about tackling this project, try to make the app _modular_ by breaking it into resusable React components.
 
 ## Example
 
-The `users` object has the user "Jane Cruz":
+```html
+<h2>Forrest Gump</h2>
+<p>Liked By:</p>
+<ul>
+  <li>Nicholas Lain</li>
+</ul>
 
-```js
-const users = {
-  1: {
-    id: 1,
-    name: 'Jane Cruz',
-    userName: 'coder',
-  }
-  .
-  .
-  .
-};
+<h2>Get Out</h2>
+<p>Liked By:</p>
+<ul>
+  <li>John Doe</li>
+  <li>Autumn Green</li>
+</ul>
+
+<h2>Autumn Green</h2>
+<p>None of the current users liked this movie</p>
 ```
-
-And the `profiles` array has a combination of which users like which movies:
-
-```js
-const profiles = [
-  {
-    id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
-  },
-  .
-  .
-  .
-];
-```
-
-From this information, one of the list items might look like this:
-
-> Jane Cruz's favorite movie is Planet Earth 1.
--- src/App.js
+++ src/App.js
@@ -98,35 +98,83 @@ const movies = {
 };
 
 class App extends Component {
-  render() {
-    profiles.forEach((elem) => {
-      console.log(
-        "elem.userID: " +
-          elem.userID +
-          " elem.favoriteMovieID: " +
-          elem.favoriteMovieID
-      );
+  constructor(props) {
+    super(props);
+    this.likes = {};
+
+    profiles.forEach((a_profile) => {
+      //create a frequency object for movie and who likes it
+      if (this.likes[a_profile.favoriteMovieID]) {
+        this.likes[a_profile.favoriteMovieID].push(a_profile.userID);
+      } else {
+        this.likes[a_profile.favoriteMovieID] = [a_profile.userID];
+      }
     });
+    console.log(this.likes);
+    //   console.log(Object.keys(this.likes));
+    //   console.log(Object.values(this.likes));
+  }
+
+  render() {
     return (
-      <div>
+      <div className="app-container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>Favorite Movies</h2>
-        <ul>
-          {profiles.map((a_profile) => {
-            return (
-              <li key={a_profile.id}>{`${
-                users[a_profile.userID].name
-              }'s favourite movie is
-                          ${movies[a_profile.favoriteMovieID].name}.`}</li>
-            );
-          })}
-        </ul>
+        <h1>Who Likes What</h1>
+        {Object.keys(this.likes).map((a_movieId) => {
+          return (
+            <div className="list-container">
+              <h2>{movies[a_movieId].name}</h2>
+              <p>Is liked by:</p>
+
+              {this.likes[a_movieId].map((a_user_id) => {
+                return (
+                  <ul>
+                    <li>{users[a_user_id].name}</li>
+                  </ul>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 export default App;
+
+// const popular = musicData
+//   .filter((a_item) => a_item.sales > 1_000_000)
+//   .map((filtered_item) => `${filtered_item.artist} is a great performer.`);
+
+{
+  /* 
+  <h2>Forrest Gump</h2>
+<p>Liked By:</p>
+<ul>
+  <li>Nicholas Lain</li>
+</ul> 
+*/
+}
+
+// {profiles.map((a_profile) => {
+//   return (
+//     <li key={a_profile.id}>{`${
+//       users[a_profile.userID].name
+//     }'s favourite movie is
+//                 ${movies[a_profile.favoriteMovieID].name}.`}</li>
+//   );
+
+// const favMovies = [];
+// profiles.forEach((a_profile) => {
+//   const item = new Object();
+//   item.theUserId = a_profile.userID;
+//   item.favMovie = a_profile.favoriteMovieID;
+//   favMovies.push(item);
+// });
+// // debug
+// favMovies.forEach((an_obj) => console.log(an_obj));
+//{theUserId: '1', favMovie: '1'}
