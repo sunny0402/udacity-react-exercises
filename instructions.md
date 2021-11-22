705afd986f30b66162745262d0b94b1f1df76c45
sunny0402 <sunnycodes0402@gmail.com>
2021-11-22 16:35:34 -0500

    controlled components starter code

--- instructions.md
+++ instructions.md
@@ -1,17 +1,9 @@
 ### Instructions
 
-You're given a starter template with dummy data.
+**Task**: Edit the code to make the printed text mirror what we type into the input
+field. When we erase all of the text, nothing should be printed to the screen.
 
-**Task**: Create a game that shows an equation of the form X+Y+Z=P. Here,
-X, Y, and Z should be random numbers, and P should be the proposed answer. The
-user should be able to answer whether it is true that the sum of X, Y, and Z
-equals the proposed answer P. The user gets a point for each question the user
-answers correctly. The score is displayed in this format: [number of correct
-answers]/[number of questions answered]. Every time the user answers a question,
-a new question that uses randomly generated numbers is displayed.
+Remember that the React component that renders the form also controls what
+happens in that form on user input.
 
-Remember that a Component's constructor is the first thing that runs when the
-object is created. The render method gets called automatically every time the state changes
-inside of the component and anytime the value of the component's props changes.
-
-This exercise will help you practice what you've learned in the course so far, including the trickiest part of React - managing state.
+This exercise will help you solidify what you've learned about Controlled Components.
--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { Component } from "react";
-import Game from "./Game";
-import Score from "./Score";
-import logo from "./logo.svg";
-import "./App.css";
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  state = {
-    correctAnswer: 0,
-    numQuestions: 0,
-  };
-
-  handleAnswer = (a_correct_answer) => {
-    if (a_correct_answer) {
-      //handle correct answer
-      this.setState((theCurrentState) => ({
-        correctAnswer: theCurrentState.correctAnswer + 1,
-      }));
-    }
-    this.setState((theCurrentState) => ({
-      numQuestions: theCurrentState.numQuestions + 1,
-    }));
-  };
   render() {
     return (
       <div className="App">
@@ -35,11 +10,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <Game handleAnswer={this.handleAnswer} />
-        <Score
-          numCorrect={this.state.correctAnswer}
-          numQuestions={this.state.numQuestions}
-        />
+        <div className="container">
+          <input type="text" placeholder="Say Something" />
+          <p className="echo">Echo:</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
       </div>
     );
   }
